2fdce178fedd8f6cef87bb6497fc3cfe44d03a33
Sarunas Dovydaitis <82829974+SarunasDov@users.noreply.github.com>
2021-10-19 20:11:40 +0300

    new updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,29 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {Prezidentas} from "./models/prezidentas";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public vardas: string;
-  public garbesTaskai: number = 0;
+  public prezidentas: Prezidentas;
+  public klases: string[] = ["geras", "blogas", "didelis"];
+
+  public klases2: Set<string> = new Set();
+
+  public patarejai: string[] = [
+    "Agnė Jakstienė",
+    "Nerijus Kaučikas",
+    "Tomas Lukoševičius",
+    "Jaroslav Neverovič"
+  ];
 
   public constructor() {
-    this.vardas = "Gitanas";
+    this.prezidentas = new Prezidentas("Gitanas");
   }
 
-  public pridetiTasku(): void {
-    this.garbesTaskai++;
-  }
-
-  public atimtiTasku(): void {
-    this.garbesTaskai--;
+  public istrintiPatareja(index: number): void {
+    this.patarejai.splice(index, 1);
   }
 }
